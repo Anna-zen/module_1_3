64d2fde6a81c6ddf8d819a6f5c70a6da44ee841f
Anna Zen <annadavidson7777@gmail.com>
2024-09-11 14:08:21 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+#Практическая работа по уроку "Динамическая типизация"
+name = 'Anna' #type string
+print ( 'Name:', name )
+age = 25 #type int
+print ('Age:', age)
+age = age + 20
+print ('New age:', age)
+is_student = True #type boolean
+print ('Is student:', is_student)
